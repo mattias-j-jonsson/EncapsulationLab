e761e4cdce7be0791662a46ad1c944c46065ecc8
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-05-06 18:48:17 +0200

    finish first task

--- Person.cs
+++ Person.cs
@@ -1,10 +1,52 @@
+using System.Runtime.CompilerServices;
+
 namespace EncapsulationLab
 {
     class Person
     {
+        private string firstName, lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get {return age;}
+            private set {age = value;}
+        }
+
         public Person(string fn, string ln, int age)
         {
-            
+            FirstName = fn;
+            LastName = ln;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
         }
     }
 }
